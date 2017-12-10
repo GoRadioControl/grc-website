87f0c2b081a849f7412f9eaf07bc2c876625111b
subhashpesh <subhash.pesh@gmail.com>
2017-12-10 10:29:02 +0530

    Added placeholders for services and workshop

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,9 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { BannerComponent } from './banner/banner.component';
 import { GetStartedComponent } from './get-started/get-started.component';
+import { WorkshopsComponent } from './workshops/workshops.component';
+import { SectionSeparatorComponent } from './section-separator/section-separator.component';
+import { ServicesComponent } from './services/services.component';
 
 
 @NgModule({
@@ -13,7 +16,10 @@ import { GetStartedComponent } from './get-started/get-started.component';
     AppComponent,
     NavbarComponent,
     BannerComponent,
-    GetStartedComponent
+    GetStartedComponent,
+    WorkshopsComponent,
+    SectionSeparatorComponent,
+    ServicesComponent
   ],
   imports: [
     BrowserModule
